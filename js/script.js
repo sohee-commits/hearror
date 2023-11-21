3d6738cca86fee967b8fe7f50e00083fbda109e7
소희 <105264974+sohee-commits@users.noreply.github.com>
2023-11-21 17:32:53 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,97 +1,59 @@
-// remove that when done with development 
-document.querySelector("#cookies").style.display = "none";
-
-// show cookies alert on load
-window.onload = function () {
-  let cookiesDialog = document.querySelector("#cookies");
-  let acceptCookies = document.querySelector("#accept-cookies");
-  let declineCookies = document.querySelector("#decline-cookies");
-  let configCookies = document.querySelector("#configure-cookies");
-  let configCookiesDialog = document.querySelector("#configuring-cookies");
-  let closeCookiesPrefs = document.querySelector("#closeCookiesPrefs");
-
-  acceptCookies.addEventListener("click", function () {
-    cookiesDialog.close();
-  });
-
-  declineCookies.addEventListener("click", function () {
-    cookiesDialog.close();
-  });
-
-  configCookies.addEventListener("click", function () {
-    configCookiesDialog.showModal();
-  });
-
-  closeCookiesPrefs.addEventListener("click", function () {
-    configCookiesDialog.close();
-    cookiesDialog.close();
-  });
-
-  cookiesDialog.show();
-};
-
 // change theme on button
-let page = document.querySelector("#page");
-let themeButton = document.querySelector("#theme-button");
+let page = document.querySelector(`#page`);
+let themeButton = document.querySelector(`#theme-button`);
 
-themeButton.addEventListener("click", function () {
-  page.classList.toggle("dark-theme");
-  page.classList.toggle("light-theme");
+themeButton.addEventListener(`click`, function () {
+  page.classList.toggle(`dark-theme`);
+  page.classList.toggle(`light-theme`);
 });
 
 // using menu button to access menu for phone screen sizes
-let menuButton = document.querySelector("#menu-button");
+let menuButton = document.querySelector(`#menu-button`);
+let menuItems = document.querySelectorAll(`.menu-item`);
+let menuItem = document.querySelector(`.menu-item`);
 
-window.addEventListener("resize", function () {
-  let width = window.innerWidth || document.documentElement.clientWidth;
-  let menu = document.querySelector("#menu");
+const mobileMenu = () => {
+  window.addEventListener(`resize`, function () {
+    let width = window.innerWidth || document.documentElement.clientWidth;
+    let menu = document.querySelector(`#menu`);
 
-  if (width <= 620) {
-    menu.classList.add("hidden");
-    menuButton.classList.remove("hidden");
-  } else {
-    menu.classList.remove("hidden");
-    menuButton.classList.add("hidden");
-  }
-});
+    if (width <= 620) {
+      // hide menu
+      menu.classList.add(`hidden`);
+
+      // show menu button
+      menuButton.classList.remove(`hidden`);
+
+      // hide menu when clicking => autoscrolling to the container
+      menuItems.forEach(function (menuItem) {
+        menuItem.addEventListener(`click`, function () {
+          menu.classList.add(`hidden`);
+        });
+      });
+    } else {
+      menu.classList.remove(`hidden`);
+      menuButton.classList.add(`hidden`);
+    }
+  });
+};
+
+window.onload = mobileMenu();
 
 // show button to scroll to the top when scrolled height of the window
-let goUpBtn = document.querySelector("#go-up");
+let goUpBtn = document.querySelector(`#go-up`);
 let height =
   window.innerHeight ||
   document.documentElement.clientHeight ||
   document.body.clientHeight;
 
-window.addEventListener("scroll", function () {
+window.addEventListener(`scroll`, function () {
   if (window.scrollY >= height) {
-    goUpBtn.classList.remove("hidden");
+    goUpBtn.classList.remove(`hidden`);
   } else {
-    goUpBtn.classList.add("hidden");
+    goUpBtn.classList.add(`hidden`);
   }
 });
 
-menuButton.addEventListener("click", function () {
-  menu.classList.toggle("hidden");
-});
-
-// business form
-
-document.querySelector("#open-business").addEventListener("click", function () {
-  document.querySelector("#business").showModal();
-});
-
-document
-  .querySelector("#close-business")
-  .addEventListener("click", function () {
-    document.querySelector("#business").close();
-  });
-
-// support form
-
-document.querySelector("#open-support").addEventListener("click", function () {
-  document.querySelector("#support").showModal();
-});
-
-document.querySelector("#close-support").addEventListener("click", function () {
-  document.querySelector("#support").close();
+menuButton.addEventListener(`click`, function () {
+  menu.classList.toggle(`hidden`);
 });
