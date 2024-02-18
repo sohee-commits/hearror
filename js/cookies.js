72ed925a6e8b7a8140aa08c9cbe3d065280f4bed
소희 <mintopiate@gmail.com>
2024-02-18 12:43:20 +0500

    Update cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -1,36 +1,32 @@
 window.onload = function () {
-  // show cookies alert on load
-  let cookies = document.querySelector(`#cookies`);
-  cookies.show();
+	// show cookies alert on load
+	let cookies = document.querySelector(`#cookies`);
+	cookies.show();
 
-  // open/close dialog buttons
-  let closeNodeList = document.querySelectorAll(`.close`);
-  let openNodeList = document.querySelectorAll(`.open`);
+	// open/close dialog buttons
+	let closeNodeList = document.querySelectorAll(`.close`);
+	let openNodeList = document.querySelectorAll(`.open`);
 
-  // close dialog
-  closeNodeList.forEach((close) => {
-    close.addEventListener(`click`, (e) => {
-      // prevent from bubbling up to parent elements
-      e.stopPropagation();
-      let dialogId = close.getAttribute(`data-dialog`);
-      let dialog = document.querySelector(`#${dialogId}`);
-      dialog.remove();
+	// close dialog
+	closeNodeList.forEach((close) => {
+		close.addEventListener(`click`, () => {
+			let dialogId = close.getAttribute(`data-dialog`);
+			let dialog = document.querySelector(`#${dialogId}`);
+			dialog.remove();
 
-      // if dialog related to cookies
-      if (dialogId === `cookies-config`) {
-        cookies.close();
-      }
-    });
-  });
+			// if dialog related to cookies
+			if (dialogId === `cookies-config`) {
+				cookies.close();
+			}
+		});
+	});
 
-  // open dialog
-  openNodeList.forEach((open) => {
-    open.addEventListener(`click`, (e) => {
-      // prevent from bubbling up to parent elements [2] lol
-      e.stopPropagation();
-      let dialogId = open.getAttribute(`data-dialog`);
-      let dialog = document.querySelector(`#${dialogId}`);
-      dialog.showModal();
-    });
-  });
+	// open dialog
+	openNodeList.forEach((open) => {
+		open.addEventListener(`click`, () => {
+			let dialogId = open.getAttribute(`data-dialog`);
+			let dialog = document.querySelector(`#${dialogId}`);
+			dialog.showModal();
+		});
+	});
 };
